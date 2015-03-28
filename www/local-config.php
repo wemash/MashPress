a428cc67c04af86929ee8c7f6feca56f6a1aeb07
Ryan Reece <ryan@mash.is>
2015-03-27 17:29:15 -0700

    Added some documentation

--- www/local-config.php
+++ www/local-config.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Here we're going to define our default database. When vagrant up is run for
+ * the first time it will run CREATE SCHEMA IF NOT EXISTS wordpress so that the
+ * database can be primed for the install.
+ */
+
 define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'root' );
